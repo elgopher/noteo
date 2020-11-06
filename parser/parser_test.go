674dccf55e4b732b3699fb75f07d4de6dec3f31c
Jacek Olszak <jacekolszak@gmail.com>
2020-11-06 12:40:51 +0100

    [refactoring] Change variable names in parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,57 +11,57 @@ import (
 
 func TestParse(t *testing.T) {
 	tests := map[string]struct {
-		content      string
-		expectedYaml string
-		expectedBody string
+		content             string
+		expectedFrontMatter string
+		expectedBody        string
 	}{
 		"empty": {},
 		"only body": {
-			content:      "text",
-			expectedYaml: "",
-			expectedBody: "text",
+			content:             "text",
+			expectedFrontMatter: "",
+			expectedBody:        "text",
 		},
 		"two lines body": {
-			content:      "one\ntwo",
-			expectedYaml: "",
-			expectedBody: "one\ntwo",
+			content:             "one\ntwo",
+			expectedFrontMatter: "",
+			expectedBody:        "one\ntwo",
 		},
-		"only yaml": {
-			content:      "---\ntags: abc\n---",
-			expectedYaml: "---\ntags: abc\n---",
-			expectedBody: "",
+		"only front matter": {
+			content:             "---\ntags: abc\n---",
+			expectedFrontMatter: "---\ntags: abc\n---",
+			expectedBody:        "",
 		},
-		"only yaml with eol": {
-			content:      "---\ntags: abc\n---\n",
-			expectedYaml: "---\ntags: abc\n---\n",
-			expectedBody: "",
+		"only front matter with eol": {
+			content:             "---\ntags: abc\n---\n",
+			expectedFrontMatter: "---\ntags: abc\n---\n",
+			expectedBody:        "",
 		},
-		"two lines yaml": {
-			content:      "---\ntags: abc\ncreated:2020-02-02\n---",
-			expectedYaml: "---\ntags: abc\ncreated:2020-02-02\n---",
-			expectedBody: "",
+		"two lines front matter": {
+			content:             "---\ntags: abc\ncreated:2020-02-02\n---",
+			expectedFrontMatter: "---\ntags: abc\ncreated:2020-02-02\n---",
+			expectedBody:        "",
 		},
-		"yaml and body": {
-			content:      "---\ntags: abc\n---\n\ntext",
-			expectedYaml: "---\ntags: abc\n---\n",
-			expectedBody: "\ntext",
+		"front matter and body": {
+			content:             "---\ntags: abc\n---\n\ntext",
+			expectedFrontMatter: "---\ntags: abc\n---\n",
+			expectedBody:        "\ntext",
 		},
-		"not closed yaml": {
-			content:      "---\ntags:abc",
-			expectedYaml: "",
-			expectedBody: "---\ntags:abc",
+		"not closed front matter": {
+			content:             "---\ntags:abc",
+			expectedFrontMatter: "",
+			expectedBody:        "---\ntags:abc",
 		},
 		"tag ---": {
-			content:      "---\ntags: ---\n---",
-			expectedYaml: "---\ntags: ---\n---",
-			expectedBody: "",
+			content:             "---\ntags: ---\n---",
+			expectedFrontMatter: "---\ntags: ---\n---",
+			expectedBody:        "",
 		},
 	}
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			yml, body, err := parser.Parse(strings.NewReader(test.content))
+			frontMatter, body, err := parser.Parse(strings.NewReader(test.content))
 			require.NoError(t, err)
-			assert.Equal(t, test.expectedYaml, yml)
+			assert.Equal(t, test.expectedFrontMatter, frontMatter)
 			assert.Equal(t, test.expectedBody, body)
 		})
 	}
