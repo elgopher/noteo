e1914adbf18285701e9c8fecd91547cb3e5fa63c
Jacek Olszak <jacekolszak@gmail.com>
2020-12-13 20:46:25 +0100

    [refactoring] Move frontMatter to a separate file

---
+++ note/frontmatter.go
@@ -0,0 +1,189 @@
+package note
+
+import (
+	"fmt"
+	"github.com/jacekolszak/noteo/date"
+	"github.com/jacekolszak/noteo/tag"
+	"gopkg.in/yaml.v2"
+	"regexp"
+	"strings"
+	"sync"
+	"time"
+)
+
+type frontMatter struct {
+	path     string
+	original func() (string, error)
+	once     sync.Once
+	mapSlice mapSlice
+	created  time.Time
+	tags     []tag.Tag
+}
+
+type mapSlice yaml.MapSlice
+
+func (s mapSlice) at(name string) (interface{}, bool) {
+	nameLowerCase := strings.ToLower(name)
+	for _, item := range s {
+		key := fmt.Sprintf("%v", item.Key)
+		key = strings.ToLower(key)
+		if key == nameLowerCase {
+			return item.Value, true
+		}
+	}
+	return nil, false
+}
+
+func (s mapSlice) set(name string, val interface{}) mapSlice {
+	nameLowerCase := strings.ToLower(name)
+	for i, item := range s {
+		key := fmt.Sprintf("%v", item.Key)
+		key = strings.ToLower(key)
+		if key == nameLowerCase {
+			s[i] = yaml.MapItem{
+				Key:   item.Key,
+				Value: val,
+			}
+			return s
+		}
+	}
+	return append(s, yaml.MapItem{
+		Key:   name,
+		Value: val,
+	})
+}
+
+func (s mapSlice) isEmpty() bool {
+	return len(s) == 0
+}
+
+func (h *frontMatter) ensureParsed() error {
+	var err error
+	h.once.Do(func() {
+		frontMatter, e := h.original()
+		if e != nil {
+			err = e
+			return
+		}
+		if e := yaml.Unmarshal([]byte(frontMatter), &h.mapSlice); e != nil {
+			err = fmt.Errorf("%s YAML front matter unmarshal failed: %v", h.path, e)
+			return
+		}
+		tags, ok := h.mapSlice.at("Tags")
+		if ok {
+			var tagsSlice []string
+			switch v := tags.(type) {
+			case string:
+				tagsSlice = tagSeparator.Split(v, -1)
+				if tagsSlice[0] == "" {
+					tagsSlice = tagsSlice[1:]
+				}
+			case []interface{}:
+				for _, s := range v {
+					tagsSlice = append(tagsSlice, fmt.Sprintf("%s", s))
+				}
+			}
+			for _, t := range tagsSlice {
+				t = strings.Trim(t, " ")
+				ta, e := tag.New(t)
+				if err != nil {
+					err = e
+					return
+				}
+				h.tags = append(h.tags, ta)
+			}
+		}
+		created, ok := h.mapSlice.at("Created")
+		if ok {
+			createdTime, e := date.ParseAbsolute(created.(string))
+			if e == nil {
+				h.created = createdTime
+			} else {
+				err = fmt.Errorf("%s parse failed: %v", h.path, e)
+			}
+		}
+	})
+	return err
+}
+
+func (h *frontMatter) Created() (time.Time, error) {
+	if err := h.ensureParsed(); err != nil {
+		return time.Time{}, err
+	}
+	return h.created, nil
+}
+
+func (h *frontMatter) Tags() ([]tag.Tag, error) {
+	if err := h.ensureParsed(); err != nil {
+		return nil, err
+	}
+	return h.tags, nil
+}
+
+func (h *frontMatter) setTag(newTag tag.Tag) error {
+	if err := h.ensureParsed(); err != nil {
+		return err
+	}
+	normalizedTag, err := newTag.MakeDateAbsolute()
+	if err == nil {
+		newTag = normalizedTag
+	}
+	for i, oldTag := range h.tags {
+		if oldTag.Name() == newTag.Name() {
+			h.tags[i] = newTag
+			return nil
+		}
+	}
+	h.tags = append(h.tags, newTag)
+	return nil
+}
+
+func (h *frontMatter) removeTag(newTag tag.Tag) error {
+	if err := h.ensureParsed(); err != nil {
+		return err
+	}
+	for i, oldTag := range h.tags {
+		if oldTag == newTag {
+			h.tags = append(h.tags[:i], h.tags[i+1:]...)
+			return nil
+		}
+	}
+	return nil
+}
+
+func (h *frontMatter) removeTagRegex(regex *regexp.Regexp) error {
+	if err := h.ensureParsed(); err != nil {
+		return err
+	}
+	for i, oldTag := range h.tags {
+		if regex.MatchString(oldTag.String()) {
+			h.tags = append(h.tags[:i], h.tags[i+1:]...)
+			return nil
+		}
+	}
+	return nil
+}
+
+func (h *frontMatter) marshal() (string, error) {
+	tags, err := h.Tags()
+	if err != nil {
+		return "", err
+	}
+	var stringTags []string
+	for _, t := range tags {
+		stringTags = append(stringTags, t.String())
+	}
+	serializedTags := strings.Join(stringTags, " ")
+	_, tagsWereGivenBefore := h.mapSlice.at("Tags")
+	if len(serializedTags) != 0 || tagsWereGivenBefore {
+		h.mapSlice = h.mapSlice.set("Tags", serializedTags)
+	}
+	marshaledBytes, err := yaml.Marshal(h.mapSlice)
+	if err != nil {
+		return "", err
+	}
+	if h.mapSlice.isEmpty() {
+		return "", nil
+	}
+	return "---\n" + string(marshaledBytes) + "---\n", nil
+}
